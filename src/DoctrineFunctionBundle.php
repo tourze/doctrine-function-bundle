20a1b88256a7352a4574b93e9b802a60af903fca
github-ci <you@example.com>
2025-06-18 23:53:50 +0800

    为 `build` 方法添加返回类型声明 `void`，提高代码规范性和类型检查支持。

--- src/DoctrineFunctionBundle.php
+++ src/DoctrineFunctionBundle.php
@@ -8,7 +8,7 @@ use Tourze\DoctrineFunctionBundle\DependencyInjection\ORMConfigurationPass;
 
 class DoctrineFunctionBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new ORMConfigurationPass());
